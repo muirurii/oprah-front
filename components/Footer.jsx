9bef01d0f0c48fe82479c6a865d95fdd604b4e7d
muirurii <pnkanuna@gmail.com>
2024-05-10 19:10:06 +0300

    finished working on the header header
    	# Please enter the commit message for your changes. Lines starting
    	# with '#' will be ignored, and an empty message aborts the commit.

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -3,9 +3,10 @@ import { useContext } from "react";
 import { Context } from "../context";
 
 const Footer = () => {
+  const {
+    state: { menu },
+  } = useContext(Context);
 
-  const {state:{menu}} = useContext(Context);
-  
   return (
     <footer className="text-gray-200 bg-black text-sm font-light">
       <section className="px-5 py-24 mx-auto flex md:items-center lg:items-start md:flex-row md:flex-nowrap flex-wrap flex-col">
@@ -14,7 +15,8 @@ const Footer = () => {
             <h1 className="text-xl text-secondary font-sec">Orpah</h1>
           </a>
           <p className="mt-2 text-sm text-gray-500">
-                After nourishment, shelter and companionship, stories are things we need most in the world.
+            After nourishment, shelter and companionship, stories are things we
+            need most in the world.
           </p>
         </article>
         <article className="flex-grow flex flex-wrap md:pl-20 -mb-10 md:mt-0 mt-10 md:text-left text-center">
@@ -83,15 +85,25 @@ const Footer = () => {
           </div>
         </article>
       </section>
-      <section className={`bg-white z-50 ${menu ? "fixed bottom-0 w-screen translate-x-full origin-right banner" : null}`}>
+      <section
+        className={`bg-white z-50 ${
+          false
+            ? "fixed bottom-0 w-screen translate-x-full origin-right banner"
+            : null
+        }`}
+      >
         <article className="py-2 px-5">
           <p className="flex items-center justify-center text-black md:text-left">
-           <span>Crafted by</span>
-            <a className="inline-block ml-2 text-secondary overflow-hidden relative z-10 my-link
+            <span>Crafted by</span>
+            <a
+              className="inline-block ml-2 text-secondary overflow-hidden relative z-10 my-link
                 after:absolute after:-left-2 after:bottom-0 after:h-[1px] after:bg-secondary after:w-full
-                after:-z-20" href="https://peterngugi.netlify.app " target="_blank ">
-                    Peter
-                </a>
+                after:-z-20"
+              href="https://peterngugi.netlify.app "
+              target="_blank "
+            >
+              Peter
+            </a>
           </p>
         </article>
       </section>
--- components/Header.jsx
+++ components/Header.jsx
@@ -10,6 +10,9 @@ import {
   BiLogoInstagram,
   BiLogoPinterestAlt,
   BiLogoTwitter,
+  BiMenu,
+  BiMenuAltLeft,
+  BiMenuAltRight,
   BiX,
 } from "react-icons/bi";
 
@@ -19,9 +22,8 @@ const HeaderLink = ({ link, text }) => {
   return (
     <Link className="" href={link}>
       <a
-        className="block py-6 w-full text-center border-b border-slate-400 hover:text-secondary transition-colors duration-300
-      cursor-pointer relative after:absolute after:bottom-0 after:left-0 after:w-full after:h-[1px] after:transition-all after:duration-300 after:pointer-events-none after:bg-secondary after:scale-0 after:origin-center after:hover:scale-100
-      
+        className="block py-3 md:py-6 w-full text-center border-b border-slate-400 hover:text-secondary transition-colors duration-300
+      cursor-pointer relative after:absolute after:bottom-0 after:left-0 after:w-full after:h-[1px] after:transition-all after:duration-300 after:pointer-events-none after:bg-secondary after:scale-0 after:origin-center after:hover:scale-100 hover:border-transparent
       "
       >
         {text}
@@ -52,167 +54,122 @@ const Header = () => {
 
   return (
     <header className="bg-white fixed top-0 text-sm left-0 w-screen z-10 flex items-center justify-between pr-4 sm:px-12 shadow shadow-gray-100 h-[120px]">
-      <h1 className="text-3xl  bg-white relative z-50 pl-4 sm:p-0 h-full w-full flex items-center justify-start text-secondary font-sec">
+      <h1 className="text-3xl  bg-white relative z-50 pl-4 sm:p-0 h-full w-full flex items-center justify-tart text-secondary font-sec">
         <Link href="/">
           <a>
             <span className="text-black">O</span>rpah
           </a>
         </Link>
       </h1>
-      {/* <nav className="hidden sm:block"> */}
-      <div className="fixed top-[120px] left-0 w-screen min-h-screen pointer-events-none flex justify-center">
-        <div className="flex max-w-[1500px] w-full h-fit">
+      <div
+        className={`fixed top-[120px] left-0 w-screen transition-all duration-300 origin-top ${
+          !menu ? "scale-100 opacity-100" : "scale-0 opacity-30"
+        } min-h-screen pointer-events-none flex justify-center`}
+      >
+        <div className="flex max-w-[1500px] w-full h-fit pointer-events-auto">
           <nav className="z-10 w-full border flex justify-center ">
             <ul
               className={`  h0-[calc(100vh - 120px)] flex flex-col
-              items-ceter pt-4 w-full bg-white shadow-md border-l-[32px] border-secondary shadow-slate-600 pointer-events-auto`}
+              items-ceter justify-between pt-4 w-full bg-white shadow-md border-l-8 md:border-l-[32px] border-secondary shadow-slate-300`}
             >
-              <li className="w-full py-4 px-2 flex items-center justify-evenly">
-                <BiLogoInstagram className="w-12 h-12 cursor-pointer hover:fill-secondary" />
-                <BiLogoFacebook className="w-12 h-12 cursor-pointer hover:fill-secondary" />
-                <BiLogoTwitter className="w-12 h-12 cursor-pointer hover:fill-secondary" />
-                <BiLogoPinterestAlt className="w-12 h-12 cursor-pointer hover:fill-secondary" />
-              </li>
-              <li className="px-4 py-16">
-                <p className="text-secondary font-sec text-6xl text-center">
-                  Exploring Worlds Through Words
-                </p>
-              </li>
-              <li className="w-full">
-                <HeaderLink link={"/"} text={"Home"} />
-              </li>
-              <li className="w-full">
-                <HeaderLink link={"blogs"} text={"Blogs"} />
-              </li>
-              <li className="w-full">
-                <HeaderLink link={"about"} text={"About"} />
-              </li>
-              <li className="w-full">
-                <HeaderLink link={"about"} text={"Contact"} />
-              </li>
-              {user.isLogged && user.role === "ADMIN" ? (
+              <div className="w-full">
                 <li className="w-full">
-                  <HeaderLink link={"new"} text={"New Blog"} />
+                  <HeaderLink link={"/"} text={"Home"} />
                 </li>
-              ) : null}
+                <li className="w-full">
+                  <HeaderLink link={"blogs"} text={"Blogs"} />
+                </li>
+                <li className="w-full">
+                  <HeaderLink link={"about"} text={"About"} />
+                </li>
+                <li className="w-full">
+                  <HeaderLink link={"about"} text={"Contact"} />
+                </li>
+                {user.isLogged && user.role === "ADMIN" ? (
+                  <li className="w-full">
+                    <HeaderLink link={"new"} text={"New Blog"} />
+                  </li>
+                ) : null}
+              </div>
+              <div className="w-full h-full flex flex-col justify-evenly items-center gap-y-3 md:gap-y-6 p-2 md:p-4">
+                <li className="">
+                  <p className="text-secondary font-sec text-3xl md:text-6xl text-center">
+                    Exploring Worlds Through Words
+                  </p>
+                </li>
+                <li className="w-full flex items-center justify-evenly">
+                  <BiLogoInstagram className="w-6 md:w-12 h-6 md:h-10 cursor-pointer hover:fill-secondary" />
+                  <BiLogoFacebook className="w-6 md:w-12 h-6 md:h-10 cursor-pointer hover:fill-secondary" />
+                  <BiLogoTwitter className="w-6 md:w-12 h-6 md:h-10 cursor-pointer hover:fill-secondary" />
+                  <BiLogoPinterestAlt className="w-6 md:w-12 h-6 md:h-10 cursor-pointer hover:fill-secondary" />
+                </li>
+              </div>
             </ul>
           </nav>
           <img
-            className="w-1/2"
+            className="hidden md:block w-1/2 object-cover cursor-pointer hover:-translate-x-[1px] hover:translate-y-[1px] "
             src="https://images.pexels.com/photos/1449667/pexels-photo-1449667.jpeg?auto=compress&cs=tinysrgb&w=1024"
             alt=""
           />
         </div>
       </div>
-      <button
-        onClick={handleToggleMenu}
-        className="sm:hidden absolute -bottom-8 z-40 h-9 p-1 right-0 rounded-bl bg-white"
-      >
-        <svg
-          className={`h-6 w-8 transition-all duration-300 ${
-            menu ? "fill-secondary" : null
-          }`}
-          style={{ enableBackground: "new 0 0 297 297" }}
-          xmlSpace="preserve"
-          x="0px"
-          y="0px"
-          viewBox="0 0 384.97 384.97"
-        >
-          <g>
-            <g id="Menu">
-              <path d="M12.03,84.212h360.909c6.641,0,12.03-5.39,12.03-12.03c0-6.641-5.39-12.03-12.03-12.03H12.03    C5.39,60.152,0,65.541,0,72.182C0,78.823,5.39,84.212,12.03,84.212z" />
-              <path d="M372.939,180.455H12.03c-6.641,0-12.03,5.39-12.03,12.03s5.39,12.03,12.03,12.03h360.909c6.641,0,12.03-5.39,12.03-12.03    S379.58,180.455,372.939,180.455z" />
-              <path d="M372.939,300.758H12.03c-6.641,0-12.03,5.39-12.03,12.03c0,6.641,5.39,12.03,12.03,12.03h360.909    c6.641,0,12.03-5.39,12.03-12.03C384.97,306.147,379.58,300.758,372.939,300.758z" />
-            </g>
-            <g></g>
-            <g></g>
-          </g>
-        </svg>
-      </button>
-      <nav
-        className={`absolute sm:hidden overflow-hidden top-0 right-0 shadow-gray-100 shadow-md rounded-bl-full 
-      ${menu ? "h-screen pt-[100px] px-4 rounded-bl-none" : "h-0"}
-       w-screen transition-all duration-500 z-30 bg-white`}
-        onClick={() => {
-          clearTimeout(smallMenuTimeout);
-          smallMenuTimeout = setTimeout(() => {
-            handleToggleMenu();
-          }, 500);
-        }}
-      >
-        <ul className="grid gap-y-2">
-          <li className="border-b border-gray-300 max-w-lg pl-1 pb-1 hover:text-secondary transition-colors duration-300">
-            <Link href="/">Home</Link>
-          </li>
-          <li className="border-b border-gray-300 max-w-lg pl-1 pb-1 hover:text-secondary transition-colors duration-300">
-            <Link href="/blogs">Blogs</Link>
-          </li>
-          <li className="border-b border-gray-300 max-w-lg pl-1 pb-1 hover:text-secondary transition-colors duration-300">
-            <Link href="/about">About</Link>
-          </li>
-          {user.isLogged && user.role === "ADMIN" ? (
-            <li className="border-b border-gray-300 max-w-lg pl-1 pb-1 hover:text-secondary transition-colors duration-300">
-              <Link href="/new">Add new</Link>
-            </li>
-          ) : null}
-          <li className="border-b border-gray-300 max-w-lg pl-1 pb-1 hover:text-secondary transition-colors duration-300">
-            <Link href="/profile">Profile</Link>
-          </li>
-          {user.isLogged ? (
-            <li className="border-b border-gray-300 max-w-lg pl-1 pb-1 hover:text-secondary transition-colors duration-300">
-              <button onClick={handleLogOut}>Log out</button>
-            </li>
-          ) : null}
-        </ul>
-      </nav>
-      {user.isLogged ? (
-        <li className="group absolute top-1/2 right-4 sm:right-12 z-[60] -translate-y-1/2 flex items-center justify-center gap-x-4">
-          <div className="h-9 w-9 flex items-center justify-center text-lg rounded-full border border-black relative after:absolute after:left-0 after:bottom-0 after:h-2 after:w-2 after:bg-secondary after:rounded-full cursor-pointer">
-            {user.profilePic.length ? (
-              <img
-                src={user.profilePic}
-                alt={user.username}
-                className="h-8 w-8 rounded-full"
-              />
-            ) : (
-              <span className="uppercase">{user.username.slice(0, 2)}</span>
-            )}
-            <div className="hidden group-hover:block absolute text-sm top-full right-0 min-w-[240px] p-3 bg-white border border-gray-200 rounded">
-              <p className="text-center">Logged in as {user.username}</p>
-              <div className="mt-4 flex justify-center gap-x-3 font-light">
-                <button
-                  onClick={handleLogOut}
-                  className="border transition-colors duration-300 hover:text-white hover:bg-secondary border-secondary py-2 px-3 rounded text-xs"
-                >
-                  Log out
-                </button>
-                <Link href="/profile">
-                  <a className="border border-secondary hover:bg-white hover:text-black transition-colors duration-300 bg-secondary py-2 px-3 rounded text-xs text-white">
-                    Profile
-                  </a>
-                </Link>
+      <div>
+        <div onClick={handleToggleMenu} className="relative z-[4000] mr-14">
+          {menu ? (
+            <BiMenuAltRight className="h-8 w-8 cursor-pointer" />
+          ) : (
+            <BiMenuAltLeft className="h-8 w-8 cursor-pointer" />
+          )}
+        </div>
+        {user.isLogged ? (
+          <li className="group absolute top-1/2 right-4 sm:right-12 z-[60] -translate-y-1/2 flex items-center justify-center gap-x-4">
+            <div className="h-9 w-9 flex items-center justify-center text-lg rounded-full border border-black relative after:absolute after:left-0 after:bottom-0 after:h-2 after:w-2 after:bg-secondary after:rounded-full cursor-pointer">
+              {user.profilePic.length ? (
+                <img
+                  src={user.profilePic}
+                  alt={user.username}
+                  className="h-8 w-8 rounded-full"
+                />
+              ) : (
+                <span className="uppercase">{user.username.slice(0, 2)}</span>
+              )}
+              <div className="hidden group-hover:block absolute text-sm top-full right-0 min-w-[240px] p-3 bg-white border border-gray-200 rounded">
+                <p className="text-center">Logged in as {user.username}</p>
+                <div className="mt-4 flex justify-center gap-x-3 font-light">
+                  <button
+                    onClick={handleLogOut}
+                    className="border transition-colors duration-300 hover:text-white hover:bg-secondary border-secondary py-2 px-3 rounded text-xs"
+                  >
+                    Log out
+                  </button>
+                  <Link href="/profile">
+                    <a className="border border-secondary hover:bg-white hover:text-black transition-colors duration-300 bg-secondary py-2 px-3 rounded text-xs text-white">
+                      Profile
+                    </a>
+                  </Link>
+                </div>
               </div>
             </div>
-          </div>
-        </li>
-      ) : (
-        <ul className="absolute z-50 bg-white top-0 right-2 sm:right-8 flex items-center h-full">
-          <Link href="/login">
-            <a>
-              <li className="mr-4 text-white hover:text-black bg-secondary hover:bg-white border border-secondary rounded py-2 px-3 transition-colors duration-300">
-                Log In
-              </li>
-            </a>
-          </Link>
-          <Link href="/signup">
-            <a>
-              <li className="mr-4 border border-secondary rounded py-2 px-3 hover:text-secondary transition-colors duration-300">
-                Sign Up
-              </li>
-            </a>
-          </Link>
-        </ul>
-      )}
+          </li>
+        ) : (
+          <ul className="absolute z-50 bg-white top-0 right-2 sm:right-8 flex items-center h-full">
+            <Link href="/login">
+              <a>
+                <li className="mr-4 text-white hover:text-black bg-secondary hover:bg-white border border-secondary rounded py-2 px-3 transition-colors duration-300">
+                  Log In
+                </li>
+              </a>
+            </Link>
+            <Link href="/signup">
+              <a>
+                <li className="mr-4 border border-secondary rounded py-2 px-3 hover:text-secondary transition-colors duration-300">
+                  Sign Up
+                </li>
+              </a>
+            </Link>
+          </ul>
+        )}
+      </div>
     </header>
   );
 };
