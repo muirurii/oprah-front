fe0617f554a98d6dfc3f1c7ee361f4cb598470ca
rotichkelvin <rotichkelvin489@gmail.com>
2024-05-11 17:28:02 +0300

    Updated hero section

--- pages/_document.js
+++ pages/_document.js
@@ -4,12 +4,9 @@ export default function Document() {
   return (
     <Html>
       <Head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Comforter+Brush&family=Poppins:wght@300;400;500;600&display=swap"
-          rel="stylesheet"
-        />
+      <link rel="preconnect" href="https://fonts.googleapis.com"/>
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
+<link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@300..700&family=Teachers:ital,wght@0,400..800;1,400..800&display=swap" rel="stylesheet"/>
         <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,47 +1,105 @@
+import { BiSearch, BiSearchAlt, BiSearchAlt2 } from "react-icons/bi";
 import HomeCategory from "../components/HomeCategory";
 import HomeCatLink from "../components/HomeCatLink";
 import Meta from "../components/Meta";
 import fetchData from "../customFunctions/fetch";
 
-export default function Home({latest,featured}) {
+const sections = [
+  {
+    heading: "Explore Diverse Topics",
+    content:
+      "Dive into a world of diverse topics, from technology trends to creative writing tips.",
+  },
+  {
+    heading: "Engage with Expert Insights",
+    content:
+      "Gain valuable insights from industry experts and thought leaders in every field.",
+  },
+  {
+    heading: "Stay Updated and Informed",
+    content:
+      "Stay updated with the latest news, trends, and insights shaping our world today.",
+  },
+  {
+    heading: "Join Our Community",
+    content:
+      "Join a vibrant community of like-minded individuals passionate about learning and growth.",
+  },
+  {
+    heading: "Start Your Journey",
+    content:
+      "Start your journey of discovery, learning, and empowerment with us today!",
+  },
+];
+
+export default function Home({ latest, featured }) {
   return (
-    <div className="py-8 min-h-screen w-screen">
-      <Meta title="Orpah | Home"/>
-      <div className="gap-y-12 flex flex-col items-center justify-center hero">
-        <h2 className="text-8xl font-secondary sm:text-2xl py-4 px-4 max-w-[600px]">
-       <p>
-       Unleashing the Hero Within: Inspire, Empower, Conquer!
-       </p>
+    <div className="py-8 w-screen">
+      <Meta title="Orpah | Home" />
+      <div className=" min-h-scren pt-16 gap-y-12 flex flex-col items-center justify-center hero">
+        <h2 className="text-2xl text-center font-sec sm:text-5xl leading-3 font-semibold py-4 px-4 max-w-[600px]">
+          <p>Unleashing the Hero Within, Inspire, Empower, Conquer!</p>
         </h2>
-  </div>
+        <form className="flex items-center justify-center max-w-[700px] w-full px-2">
+          <input
+            className="inline-block h-12 md:h-14 w-full home-input pl-4 outline-none placeholder:text-center placeholder:text-sm pl md:placeholder:text-md
+            border rounded-tl-full rounded-bl-full focus:border-2 border-secondary"
+            placeholder="Search for articles, tips, and inspiration"
+            type="text"
+          />
+          <button className="p-2 md:p-3 hover:fill-white bg-secondary border-l-0 rounded-tr-full rounded-br-full">
+            <BiSearch className="h-8 w-8 stroke-white hover:stroke-slate-200  stroke-[2px]" />
+          </button>
+        </form>
+        <div className=""></div>
+      </div>
+      <section
+        className="py-8 flex justify-evenly items-center mb-48 h relative 
+      after:absolute after:top-8 after:left-8 after:h-24 after:w-24 after:bg-secondary
+      before:absolute before:-bottom-40 before:right-8 before:h-24 before:w-24 before:bg-secondary
+      "
+      >
+        <img
+          className="h-[700px] w-[400px] object-cover rounded-xl relative top-48"
+          src="https://images.pexels.com/photos/5965896/pexels-photo-5965896.jpeg?auto=compress&cs=tinysrgb&w=600"
+          alt=""
+        />
+        <section>
+          {sections.map((section, index) => (
+            <section key={index} className="py-4">
+              <h1 className="text-secondary text-2xl font-sec pb-4">
+                {section.heading}
+              </h1>
+              <p className="max-w-[300px]">{section.content}</p>
+            </section>
+          ))}
+        </section>
+        <img
+          className="h-[700px] w-[400px] object-cover rounded-xl"
+          src="https://images.pexels.com/photos/3771127/pexels-photo-3771127.jpeg?auto=compress&cs=tinysrgb&w=600"
+          alt=""
+        />
+      </section>
       <HomeCategory posts={latest} heading="Latest blogs" />
       <HomeCategory posts={featured} heading="Popular blogs" />
-      {/* <section className="my-8">
-        <h1 className="text-3xl pl-4">Explore More</h1>
-          <ul className="flex flex-wrap items-center justify-evenly p-8 my-8 gap-4">
-            <HomeCatLink text="lifestyle" />
-            <HomeCatLink text="fashion" />
-            <HomeCatLink text="technology" />
-          </ul>
-      </section> */}
     </div>
   );
 }
 
-export const getServerSideProps = async () =>{
-    const res = await fetchData('posts/featured',"GET");
+export const getServerSideProps = async () => {
+  const res = await fetchData("posts/featured", "GET");
 
-    if(res.status !== 200){
-      return {
-        notFound:true
-      }
-    }
-    const posts = await res.json();
+  if (res.status !== 200) {
+    return {
+      notFound: true,
+    };
+  }
+  const posts = await res.json();
 
-    return{
-      props:{
-        featured:posts.featured,
-        latest:posts.latest,
-      }
-    }
-}
+  return {
+    props: {
+      featured: posts.featured,
+      latest: posts.latest,
+    },
+  };
+};
